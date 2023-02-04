256cd1bbe87dc64137d93931e47b904b752f55d9
Dariaaal <1406dariaal@gmail.com>
2023-02-04 22:59:53 +0200

    import

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import axios from 'axios';
+import infiniteScroll from 'infinite-scroll';
