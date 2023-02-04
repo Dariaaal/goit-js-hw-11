27df90f39af2c20aebd56e2a81219393592a2257
Dariaaal <1406dariaal@gmail.com>
2023-02-05 00:40:56 +0200

    get elements

--- src/index.js
+++ src/index.js
@@ -4,3 +4,21 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 import axios from 'axios';
 import infiniteScroll from 'infinite-scroll';
 import imagesEl from './fetchImages';
+
+const form = document.querySelector('.search-form');
+const inputEl = document.querySelector('input');
+const searchBtn = document.querySelector('button');
+const imagesList = document.querySelector('.gallery');
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+    const inputValue = e.target.value;
+
+    imagesEl.fetchImages(inputValue).then(image=>{})
+}
+
+function createImageCard() {
+    
+}
